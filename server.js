adc4c962c72ea4a36ffe3513b03f55b7743274af
TatianaRohovets <tnr78@ukr.net>
2023-06-30 20:45:30 +0300

    connectDB_HOST

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('./app')
+const app = require('./app');
+require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
+const { DB_HOST } = process.env
+console.log(DB_HOST)
+const mongoose = require('mongoose');
+
+
+mongoose.connect(DB_HOST).then(() => console.log('DB Connected')).catch((error) => console.error(err.message));
