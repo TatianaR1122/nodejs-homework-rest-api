bd0e9a36fd240e6ea520cfdb27e38cd8894ce2b4
TatianaRohovets <tnr78@ukr.net>
2023-07-06 10:13:29 +0300

    usermodel

--- database/models/contact.model.js
+++ database/models/contact.model.js
@@ -18,6 +18,11 @@ const contactSchema = mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true }
 );
