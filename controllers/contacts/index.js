df7bb86db292a7d6cd535d20ae15a93e7736608a
TatianaRohovets <tnr78@ukr.net>
2023-07-07 15:07:03 +0300

    logout

--- controllers/contacts/index.js
+++ controllers/contacts/index.js
@@ -1,15 +1,15 @@
-// const { getAll } = require('./get-all');
-// const { getById } = require('./get-by-id');
-// const { addContacts } = require('./add-contact');
-// const { updateById } = require('./update-contact');
-// const { deleteById } = require('./delete-contact');
-// const { updateStatusContact } = require('./update-favorite');
+const { getAll } = require('./get-all');
+const { getById } = require('./get-by-id');
+const { addContacts } = require('./add-contact');
+const { updateById } = require('./update-contact');
+const { deleteById } = require('./delete-contact');
+const { updateStatusContact } = require('./update-favorite');
 
-// module.exports = {
-//   getAll,
-//   getById,
-//   addContacts,
-//   updateById,
-//   deleteById,
-//   updateStatusContact,
-// };
+module.exports = {
+  getAll,
+  getById,
+  addContacts,
+  updateById,
+  deleteById,
+  updateStatusContact,
+};
