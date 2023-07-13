5c5186b40b2736b9023e3965bdf4a7414a6016db
TatianaRohovets <tnr78@ukr.net>
2023-07-13 13:05:20 +0300

    step1

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(morgan(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api/auth', authRouter);
 app.use('/api/contacts', contactsRouter);
